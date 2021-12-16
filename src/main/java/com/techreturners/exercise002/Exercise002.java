58d7c3ddbc5993a5b4c1cc39615061b601943b34
Sona Sugumaran <92852374+mailtosona@users.noreply.github.com>
2021-12-16 17:37:35 +0000

    Update Exercise002.java

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,20 +2,10 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
     public boolean isFromManchester(Person person) {
-        if (person.getCity().equals("Manchester")) {
-            return true;
-        } else {
-            return false;
-        }
+        return person.getCity().equals("Manchester");
     }
 
     public boolean canWatchFilm(Person person, int Age) {
-        if (person.getAge() >= 18) {
-
-            return true;
-        } else {
-
-            return false;
-        }
+       return person.getAge() >= 18;
     }
 }
